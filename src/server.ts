c0d535e9497cb04d16b99597fa5b99fdab55e9d1
Danilo Souza <danilo_souza19@outlook.com>
2021-05-24 09:34:28 -0300

    add prettier

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello world!!' }));
+app.get('/', (request, response) =>
+  response.json({ message: 'Hello world!!' })
+);
 
 app.listen(3333, () => {
   console.log('server is running on port 3333');
