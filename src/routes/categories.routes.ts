0881f07823ebfd47956f7ce30bf3dd10b633ea86
Danilo Souza <danilo_souza19@outlook.com>
2021-05-24 14:39:52 -0300

    add id as uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
@@ -7,10 +8,13 @@ const categories = [];
 categoriesRoutes.post('/categories', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = {
+    id: uuidv4(),
     name,
     description,
-  });
+  };
+
+  categories.push(category);
 
   return response.status(201).send();
 });
