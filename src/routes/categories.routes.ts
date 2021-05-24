3af092ff2aad723fe26a9d266e9e3e1af2466071
Danilo Souza <danilo_souza19@outlook.com>
2021-05-24 15:35:17 -0300

    add list Categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -4,10 +4,10 @@ import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
 
+const categoriesRepository = new CategoriesRepository();
+
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
-
-  const categoriesRepository = new CategoriesRepository();
 
   categoriesRepository.create({
     description,
@@ -15,6 +15,12 @@ categoriesRoutes.post('/', (request: Request, response: Response) => {
   });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request: Request, response: Response) => {
+  const categories = categoriesRepository.list();
+
+  return response.json(categories);
 });
 
 export { categoriesRoutes };
