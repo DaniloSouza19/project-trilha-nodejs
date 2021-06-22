7bfe4f0dd00dd77b6de4680fe5e2124d16492853
Danilo Souza <danilo_souza19@outlook.com>
2021-06-22 07:13:12 -0300

    add entity of users

---
+++ src/modules/accounts/entities/Users.ts
@@ -0,0 +1,30 @@
+import { Entity, Column, CreateDateColumn, PrimaryColumn } from 'typeorm';
+
+@Entity('users')
+class Users {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  driver_license: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
+
+export { Users };
