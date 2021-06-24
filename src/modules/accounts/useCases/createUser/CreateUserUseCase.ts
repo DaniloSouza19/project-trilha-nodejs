540a30de22c07460db06a12eaf7d262a8ead65a9
Danilo Souza <danilo_souza19@outlook.com>
2021-06-24 06:46:22 -0300

    fix | make class injectable

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -3,6 +3,7 @@ import { inject } from 'tsyringe';
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
+@injectable()
 class CreateUserUseCase {
   constructor(
     @inject('UsersRepository')
