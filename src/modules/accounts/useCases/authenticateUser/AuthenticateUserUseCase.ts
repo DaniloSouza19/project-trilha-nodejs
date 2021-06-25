4d53107ff5f111158dc17f9ce32f8edd77440a6a
Danilo Souza <danilo_souza19@outlook.com>
2021-06-25 06:46:54 -0300

    add authenticate user useCase

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,55 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  token: string;
+  user: {
+    name: string;
+    email: string;
+  };
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error('Email or password does not match');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Email or password does not match');
+    }
+
+    const token = sign({}, 'd9a0b28f86c64b71216964dd3d2ca23b', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      token,
+      user: {
+        email: user.email,
+        name: user.name,
+      },
+    };
+  }
+}
+
+export { AuthenticateUserUseCase };
