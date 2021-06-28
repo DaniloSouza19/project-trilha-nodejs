fef9db5c45e5941a89146ca0fcf0adb1e48786ed
Danilo Souza <danilo_souza19@outlook.com>
2021-06-28 06:52:23 -0300

    app middware to handle with errors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
 import 'reflect-metadata';
@@ -7,6 +8,7 @@ import './shared/container';
 import { router } from './routes';
 import swaggerDocument from './swagger.json';
 import './database';
+import { AppError } from './errors/AppError';
 
 const app = express();
 
@@ -15,6 +17,21 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(router);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message,
+      });
+    }
+    console.log(error);
+
+    return response.status(500).json({
+      message: 'internal server Error',
+    });
+  }
+);
 
 app.listen(3333, () => {
   console.log('server is running on port 3333');
