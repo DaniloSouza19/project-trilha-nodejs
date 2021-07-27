9eb78e65ec96108cd2fc77a19578db4f51670a3c
Danilo Souza <danilo_souza19@outlook.com>
2021-07-27 06:26:24 -0300

    Revert "delete old file before update user avatar"
    This reverts commit 1578e458fc929d2b433374ed4d80a3d2fb9953b8.

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,5 @@
 import { inject, injectable } from 'tsyringe';
 
-import { deleteFile } from '../../../../utils/file';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
@@ -17,10 +16,6 @@ class UpdateUserAvatarUseCase {
 
   async execute({ avatar_file, user_id }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id);
-
-    if (user.avatar) {
-      await deleteFile(`./tmp/avatar/${user.avatar}`);
-    }
 
     user.avatar = avatar_file;
 
