6b2e2fa96258f6d828342a0b6016f38b7ae8d8f3
Danilo Souza <danilo_souza19@outlook.com>
2021-08-03 11:54:26 -0300

    change imports to paths

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,9 @@ import swaggerUi from 'swagger-ui-express';
 
 import 'reflect-metadata';
 
-import './shared/container';
-import { AppError } from './errors/AppError';
+import '@shared/container';
+import { AppError } from '@errors/AppError';
+
 import { router } from './routes';
 import swaggerDocument from './swagger.json';
 import './database';
