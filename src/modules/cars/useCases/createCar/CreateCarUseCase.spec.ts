ecaf3c400e4cf82ee8b7642323fd5addfd232b27
Danilo Souza <danilo_souza19@outlook.com>
2021-08-26 17:05:28 -0300

    add tests: available true and non existing licence

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRespositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
 
 import { CreateCarUseCase } from './CreateCarUseCase';
 
@@ -12,7 +13,7 @@ describe('Create car', () => {
   });
 
   it('Should be able to create a car', async () => {
-    await createCarUseCase.execute({
+    const car = await createCarUseCase.execute({
       name: 'Car name',
       brand: 'Car brand',
       category_id: '445454',
@@ -20,5 +21,42 @@ describe('Create car', () => {
       description: 'Description car',
       licence_plate: 'ABC7788',
     });
+
+    expect(car).toHaveProperty('id');
+  });
+
+  it('Should not be able to create a car with an already existing license plate', async () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: 'Car 1',
+        brand: 'Car brand',
+        category_id: '445454',
+        daily_rate: 100,
+        description: 'Description car',
+        licence_plate: 'ABC7788',
+      });
+
+      await createCarUseCase.execute({
+        name: 'Car 2',
+        brand: 'Car 2 brand',
+        category_id: '445454',
+        daily_rate: 100,
+        description: 'Car 2 Description',
+        licence_plate: 'ABC7788',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should be able to create a car with availability true by defaults', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Car name',
+      brand: 'Car brand',
+      category_id: '445454',
+      daily_rate: 100,
+      description: 'Description car',
+      licence_plate: 'ABC7788',
+    });
+
+    expect(car.available).toBe(true);
   });
 });
