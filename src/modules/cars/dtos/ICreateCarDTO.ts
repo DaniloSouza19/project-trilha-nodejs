27584a0bb5269e13c951d718fd021cb25e15093a
Danilo Souza <danilo_souza19@outlook.com>
2021-08-26 16:35:22 -0300

    add ICreateCar DTO

---
+++ src/modules/cars/dtos/ICreateCarDTO.ts
@@ -0,0 +1,9 @@
+interface ICreateCarDTO {
+  name: string;
+  description: string;
+  daily_rate: number;
+  licence_plate: string;
+  brand: string;
+  category_id: string;
+}
+export { ICreateCarDTO };
