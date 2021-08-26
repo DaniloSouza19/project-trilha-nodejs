5a79fa6ed136611237e8df40b7fd63efdc65b22d
Danilo Souza <danilo_souza19@outlook.com>
2021-08-26 16:34:35 -0300

    add entity car

---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,40 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('cars')
+class Car {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  daily_rate: number;
+
+  @Column()
+  available: boolean;
+
+  @Column()
+  licence_plate: string;
+
+  @Column()
+  brand: string;
+
+  @Column()
+  category_id: string;
+
+  @Column()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
+
+export { Car };
