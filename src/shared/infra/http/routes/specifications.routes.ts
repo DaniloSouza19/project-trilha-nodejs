d63f7c257766a22eb62dd7a5214498fc6762580d
Danilo Souza <danilo_souza19@outlook.com>
2021-11-16 15:50:59 -0300

    add middeware ensureAdmin

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -2,10 +2,16 @@ import { Router } from 'express';
 
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
 
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.post('/', createSpecificationController.handle);
+specificationsRoutes.post(
+  '/',
+  ensureAdmin,
+  createSpecificationController.handle
+);
 
 export { specificationsRoutes };
