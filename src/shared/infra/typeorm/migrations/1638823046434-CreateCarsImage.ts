77a538fe43d6cbdd4f1c50d61bafe501226094c0
Danilo Souza <danilo_souza19@outlook.com>
2021-12-06 17:49:05 -0300

    add createCarsImage migration

---
+++ src/shared/infra/typeorm/migrations/1638823046434-CreateCarsImage.ts
@@ -0,0 +1,50 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateCarsImage1638823046434 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'cars_image',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'car_id',
+            type: 'uuid',
+          },
+          {
+            name: 'image_name',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKCarImage',
+            columnNames: ['car_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'cars',
+            onDelete: 'SET NULL',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('cars_image');
+  }
+}
