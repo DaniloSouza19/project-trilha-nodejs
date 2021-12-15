f821ef96d04ee150657499a08dc7a828c47bfd8f
Danilo Souza <danilo_souza19@outlook.com>
2021-12-15 17:25:10 -0300

    add test expected return date before 24 hours

--- src/modules/rentals/useCases/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/CreateRentalUseCase.spec.ts
@@ -1,3 +1,6 @@
+import { addHours } from 'date-fns';
+
+import { DateFnsDateProvider } from '@shared/container/providers/dateProvider/implementations/DateFnsDateProvider';
 import { AppError } from '@shared/errors/AppError';
 
 import { RentalsRepositoryInMemory } from '../repositories/in-memory/RentalsRepositoryInMemory';
@@ -5,17 +8,22 @@ import { CreateRentalUseCase } from './CreateRentalUseCase';
 
 let createRentalUseCase: CreateRentalUseCase;
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+let dateFnsDateProvider: DateFnsDateProvider;
 
 describe('Create a Rental', () => {
   beforeEach(() => {
     rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
-    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+    dateFnsDateProvider = new DateFnsDateProvider();
+    createRentalUseCase = new CreateRentalUseCase(
+      rentalsRepositoryInMemory,
+      dateFnsDateProvider
+    );
   });
 
   it('Should be able to create a rental', async () => {
     const rental = await createRentalUseCase.execute({
       car_id: 'some-car-id',
-      expected_return_date: new Date(),
+      expected_return_date: addHours(new Date(), 25),
       user_id: 'some-user-id',
     });
 
@@ -26,13 +34,13 @@ describe('Create a Rental', () => {
     await expect(async () => {
       await createRentalUseCase.execute({
         car_id: 'some-car',
-        expected_return_date: new Date(),
+        expected_return_date: addHours(new Date(), 25),
         user_id: 'same-user-test',
       });
 
       await createRentalUseCase.execute({
         car_id: 'another-car',
-        expected_return_date: new Date(),
+        expected_return_date: addHours(new Date(), 25),
         user_id: 'same-user-test',
       });
     }).rejects.toBeInstanceOf(AppError);
@@ -42,13 +50,23 @@ describe('Create a Rental', () => {
     await expect(async () => {
       await createRentalUseCase.execute({
         car_id: 'same-car-test',
-        expected_return_date: new Date(),
+        expected_return_date: addHours(new Date(), 25),
         user_id: 'some-user',
       });
 
       await createRentalUseCase.execute({
         car_id: 'same-car-test',
-        expected_return_date: new Date(),
+        expected_return_date: addHours(new Date(), 25),
+        user_id: 'another-user',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should not be able to create a rental with expected return date before 24 hours', async () => {
+    await expect(async () => {
+      await createRentalUseCase.execute({
+        car_id: 'same-car-test',
+        expected_return_date: addHours(new Date(), 23),
         user_id: 'another-user',
       });
     }).rejects.toBeInstanceOf(AppError);
