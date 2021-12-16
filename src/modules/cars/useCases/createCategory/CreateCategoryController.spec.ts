f49301593dacb3c78b823eb78acdd0fa8aaf49d9
Danilo Souza <danilo_souza19@outlook.com>
2021-12-16 14:04:25 -0300

    add HTTP integration test for CreateCategory

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,81 @@
+import { hash } from 'bcrypt';
+import request from 'supertest';
+import { Connection } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+let connection: Connection;
+
+describe('Create category', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const password = await hash('admin', 8);
+
+    await connection.query(`INSERT INTO users(id, name, password, email, driver_license, "isAdmin", created_at) 
+    VALUES ('${id}', 'admin', '${password}', 'admin@rentx.com.br', 'xxxxxx', true, 'now()')`);
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('Should be able to create category', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@rentx.com.br',
+      password: 'admin',
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Some Category',
+        description: 'Some description',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.statusCode).toBe(201);
+  });
+
+  it('Should not be able to create a new category with an existing name', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@rentx.com.br',
+      password: 'admin',
+    });
+
+    const { token } = responseToken.body;
+
+    await request(app)
+      .post('/categories')
+      .send({
+        name: 'Some Category',
+        description: 'Some description',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Some Category',
+        description: 'Some description',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toEqual('Category already exits');
+  });
+});
