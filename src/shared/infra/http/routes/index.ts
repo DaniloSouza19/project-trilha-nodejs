5e4520f2861f692d4e8df5fbf4bd9985123f2b1c
Danilo Souza <danilo_souza19@outlook.com>
2021-12-16 08:57:17 -0300

    add rentals routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
-import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { authenticateRoutes } from './authenticate.routes';
 import { carsRoutes } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
+import { rentalsRoutes } from './rentals.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
 
@@ -16,5 +16,6 @@ router.use('/users', usersRoutes);
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
 router.use('/cars', carsRoutes);
+router.use('/rentals', rentalsRoutes);
 
 export { router };
